f040808e6e0e9e899771774db81fd6a41db703f0
mika de vries <mikadevries34@outlookcom>
2020-03-20 11:04:32 +0100

    Add project files.

---
+++ BasicWebProject/App_Code/Songmethods.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicWebProject.App_Code
+{
+    using System.Data;
+    public class Songmethods
+    {
+        private DataSet ds = new DataSet("playlist");
+        public string filename;
+
+        public Songmethods()
+        {
+
+        }
+
+        public DataSet GetAllSongs(string Filename)
+        {
+            filename = filename;
+            DataTable dtSong = new DataTable("song");
+
+            DataColumn dcId = new DataColumn("id");
+            DataColumn dcTitel = new DataColumn("titel");
+            DataColumn dcArtist = new DataColumn("artist");
+
+            dtSong.Columns.Add(dcTitel);
+            dtSong.Columns.Add(dcArtist);
+            dtSong.Columns.Add(dcId);
+
+            ds.Tables.Add(dtSong);
+
+            ds.ReadXml(HttpContext.Current.Server.MapPath(Filename));
+            return ds;
+        }
+
+        public void CreateSong(DataRow datarow,string file)
+        {
+            ds.Tables["song"].Rows.Add(datarow);
+            ds.WriteXml(HttpContext.Current.Server.MapPath(file));
+        }
+
+        public void DeleteSong(string id , string filename)
+        {
+            DataRow[] drarray = ds.Tables["song"].Select("id = '" + id + "'");
+            if (drarray != null && drarray.Length > 0)
+            {
+                drarray[0].Delete();
+                ds.WriteXml(HttpContext.Current.Server.MapPath(filename));
+            }
+        }
+
+        public DataRow GetEmptyDataRow()
+        {
+            DataRow dr = ds.Tables["song"].NewRow();
+            return dr;
+        }
+
+        public void SavaRow (DataRow dr)
+        {
+            ds.Tables["song"].Rows.Add(dr);
+            ds.WriteXml(HttpContext.Current.Server.MapPath(filename));
+        }
+        /*public DataSet GetALLSongs(string filename)
+        {
+            DataSet ds = new DataSet("playlist");
+
+            DataTable dt = new DataTable("song");
+
+            DataColumn dcId = new DataColumn("id");
+            DataColumn dcTitel = new DataColumn("titel");
+            DataColumn dcArtist = new DataColumn("artist");
+
+            dt.Columns.Add(dcTitel);
+            dt.Columns.Add(dcArtist);
+            dt.Columns.Add(dcId);
+
+            ds.Tables.Add(dt);
+
+            ds.ReadXml(HttpContext.Current.Server.MapPath(filename));
+            return ds;
+        }*/
+    }
+}
---
+++ BasicWebProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BasicWebProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicWebProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("85ef6825-17b7-420c-8020-93773729bf99")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
